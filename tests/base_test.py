91338703a113cc79dfa25b796310b6c43c5e191f
Benjamin Oddou <benjaminoddou@gmail.com>
2023-05-18 00:24:38 +0200

    Update base_test

--- tests/base_test.py
+++ tests/base_test.py
@@ -2,8 +2,9 @@
 
 import sys
 sys.path.insert(0, './lib')
-from lib.plexapi.server import PlexServer
-from lib.plexapi import utils
+from plexapi.myplex import MyPlexAccount
+from plexapi.server import PlexServer
+from plexapi import utils
 from utils import servers_file
 
 data = servers_file()
@@ -12,6 +13,7 @@ obj = data['items'][0] # first server used
 baseURL = obj.get('baseURL')
 plexToken = obj.get('plexToken')
 try:
+    plex_account = MyPlexAccount(plexToken)
     plex_instance = PlexServer(baseURL, plexToken)
 except:
     print('error')
@@ -19,3 +21,20 @@ except:
 
 test = plex_instance.search('Harry Potter', mediatype='movie')
 print(f'{baseURL}{test[0].media[0].parts[0].key}?download=1&X-Plex-Token={plexToken}')
+
+headers = {
+    'Accept': 'application/json'
+}
+params = {
+    'query': 'zendaya',
+    'limit': 30,
+    'searchTypes': 'people',
+    'includeMetadata': 1,
+    'filterPeople': 1
+}
+
+test2 = plex_account.query('https://metadata.provider.plex.tv/library/search?', headers=headers, params=params)
+print(test2)
+
+test3 = plex_instance.search('zendaya')
+print(test3[0]._data.attrib)
